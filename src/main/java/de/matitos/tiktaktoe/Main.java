e0a390ac8e27a0d9022048620a65659ac850efdd
Matito <matitos@outlook.de>
2022-11-12 00:38:01 +0100

    Base Structure
    - MC Version
    - Startup logging

--- src/main/java/de/matitos/tiktaktoe/Main.java
+++ src/main/java/de/matitos/tiktaktoe/Main.java
@@ -1,17 +1,35 @@
 package de.matitos.tiktaktoe;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        final Properties properties = new Properties();
+        try {
+            properties.load(this.getClassLoader().getResourceAsStream("project.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Bukkit.getConsoleSender().sendMessage(getPrefix() + "");
+        Bukkit.getConsoleSender().sendMessage(getPrefix() + properties.getProperty("plugin") + "enabled");
+        Bukkit.getConsoleSender().sendMessage(getPrefix() + "Version: " + properties.getProperty("version"));
+        Bukkit.getConsoleSender().sendMessage(getPrefix() + "");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+    public String getPrefix(){
+        return ChatColor.DARK_GRAY + "[" + ChatColor.DARK_GREEN + "TicTakToe" + ChatColor.DARK_GRAY + "] " + ChatColor.DARK_AQUA;
     }
 }
